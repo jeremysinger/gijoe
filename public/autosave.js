0e7b7cc075a9f9d8f061e522c165775ac1e67a73
Robbert Sinclair <2432710S@student.gla.ac.uk>
2021-07-09 11:19:58 +0100

    Managed to get a code snippet into the localStorage

--- public/autosave.js
+++ public/autosave.js
@@ -3,11 +3,15 @@ Autosave feature for the gijoe repo. For now I'm gonna make use of the localStor
 before I understand where the data will be going. I assume that there will be a database
 somewhere that we can use in order.
 */
-console.log("Autosave loaded successfully");
 
 setInterval(autosave, 2000);
 var codeArea = document.getElementById("textareacode");
+myCodeMirror.setValue(localStorage.getItem("code"));
 
 function autosave() {
-    alert("Autosaved");
+    var theValue = codeArea.value;
+    localStorage.setItem("code", theValue);
+    var codeFragment = myCodeMirror.getValue();
+    console.log(codeFragment);
+    localStorage.setItem("code", codeFragment);
 }
