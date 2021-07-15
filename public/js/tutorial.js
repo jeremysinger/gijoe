c964c279fabdd23018dd590d2ccef96d8026e7e3
Robbert Sinclair <2432710S@student.gla.ac.uk>
2021-07-15 11:51:08 +0100

    Made a basic tutorial function

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -1,1 +1,55 @@
-console.log("Tutorial Loaded Successfully");
+//GET THE TUTORIAL FILE
+
+const forwardButton = document.getElementById("tutorialForward");
+const backButton = document.getElementById("tutorialBack");
+
+
+function getTutorial(id) {
+    const http = new XMLHttpRequest();
+    http.open("GET", `/tutorial/${id}`);
+
+    http.onload = function() {
+        
+        if (http.status === 200) {
+            let data = JSON.parse(http.responseText);
+            let steps =  data.steps;
+            myCodeMirror.setValue(steps[0]);
+            let i = 1;
+            
+            forwardButton.addEventListener("click", () => {
+                if(backButton.disabled) {
+                    backButton.disabled = false;
+                }
+                const tutorialText = steps.slice(0, i+1).join("//NEXT");
+                myCodeMirror.setValue(tutorialText);
+                i++;
+                if (i == steps.length) {
+                    forwardButton.disabled = true;
+                }
+            });
+
+            backButton.addEventListener("click", () => {
+                if (forwardButton.disabled) {
+                    forwardButton.disabled = false;
+                }
+                const tutorialText = steps.slice(0, i - 1).join("//NEXT");
+                myCodeMirror.setValue(tutorialText);
+                i--;
+                if (i == 0) {
+                    backButton.disabled = true;
+                }
+            });
+
+            
+            
+
+        } else {
+            console.log("DATA NOT FOUND");
+        }
+        
+    }
+
+    http.send();
+}
+
+getTutorial(1);
