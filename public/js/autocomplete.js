f88343918d70e7e05215a5f1480391178feec661
Robbert Sinclair <2432710S@student.gla.ac.uk>
2021-07-20 15:00:37 +0100

    Added a form of autocomplete to the codemirror elements

---
+++ public/js/autocomplete.js
@@ -0,0 +1,9 @@
+
+
+myCodeMirror.on("keyup", (cm, event) => {
+    if (!cm.state.completionActive && event.keyCode != 13) {
+        CodeMirror.commands.autocomplete(cm, null, {completeSingle: false});
+    }
+    
+    
+})
