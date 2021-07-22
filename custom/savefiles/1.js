8c46e789ca46c051373ca8282c59164644ea0dc6
Robbert Sinclair <2432710S@student.gla.ac.uk>
2021-07-22 20:51:16 +0100

    Got rid of the tutorial code mirror

--- custom/savefiles/1.js
+++ custom/savefiles/1.js
@@ -4,6 +4,7 @@ console.log("Hello World!");
 console.log("Hello World");
 console.log("Hello World");
 console.log("Write to 1.js");
+console.log("I need to fix that");
 // end JavaScript code
 
 console.log("Hello World");
