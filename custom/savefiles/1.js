2841ecf9857512d7f4e623447653709ab067952c
Robbert Sinclair <2432710S@student.gla.ac.uk>
2021-07-22 14:49:36 +0100

    Moved the autosave message

--- custom/savefiles/1.js
+++ custom/savefiles/1.js
@@ -1,7 +1,10 @@
 // sample JavaScript code
 
 console.log("Hello World!");
-
 console.log("Hello World");
-console.log("Write to 1.js")
+console.log("Hello World");
+console.log("Write to 1.js");
+dsjfsofjdiofjisfojsdifosdjiosj
 // end JavaScript code
+
+console.log("Hello World")
