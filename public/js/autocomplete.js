0342635048d3b6487d004990fc7ad91bd066012b
Robbert Sinclair <2432710S@student.gla.ac.uk>
2021-07-25 17:20:59 +0100

    Added some comments to help understand the autocomplete code

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -1,5 +1,19 @@
 myCodeMirror.on("keyup", (cm, event) => {
-    const excludedKeys = [8, 9, 13, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 186, 219, 221];
+    /*
+    Get the keys which are excluded from the auto complete functions
+    these are:
+    8   - Backspace
+    9   - Tab
+    13  - Enter
+    48  - )
+    57  - (
+    186 - ; 
+    219 - {
+    221 - }
+
+    This will hopefully fix the annoying part of the auto complete function
+    */
+    const excludedKeys = [8, 9, 13, 48, 57, 186, 219, 221];
     if (!cm.state.completionActive && !excludedKeys.includes(event.keyCode)) {
         CodeMirror.commands.autocomplete(cm, null, {completeSingle: false});
     }
