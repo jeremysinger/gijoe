58189ca51bf48821ce67827c3fe99e41037941b0
Robbert Sinclair <2432710S@student.gla.ac.uk>
2021-07-25 17:08:07 +0100

    Improved the auto complete function

--- custom/savefiles/1.js
+++ custom/savefiles/1.js
@@ -1,3 +1,15 @@
 // sample JavaScript code
 
-console.log();
+for (var i = 0; i < 1000; i++) {
+  	left(90);
+	forward(i);
+	left(45);
+	forward(i);
+	right(20);
+	forward(i);
+	left(30);
+	forward(i);
+  	left(25);
+	forward(i); 
+}
+
--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -1,5 +1,6 @@
 myCodeMirror.on("keyup", (cm, event) => {
-    if (!cm.state.completionActive && event.keyCode != 13) {
+    const excludedKeys = [8, 9, 13, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 186, 219, 221];
+    if (!cm.state.completionActive && !excludedKeys.includes(event.keyCode)) {
         CodeMirror.commands.autocomplete(cm, null, {completeSingle: false});
     }
-});
+})
