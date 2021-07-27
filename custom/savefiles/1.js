42e9840b28715c3ef90ea0c0f98d02bb76cb888e
ddrago <dragodiego286@gmail.com>
2021-07-27 20:38:13 +0100

    now if the code executed has an error the user is alerted about it

--- custom/savefiles/1.js
+++ custom/savefiles/1.js
@@ -1,16 +1,6 @@
 // sample JavaScript code
 
-for (var i = 1; i < 100; i++) {
-  	left(90);
-	forward(i);
-	left(45);
-	forward(i);
-	right(20);
-	forward(i);
-	left(30);
-	forward(i);
-  	left(25);
-	forward(i);
-  	console.log("Hello World");
-}
+log("hello");
+forward(i);
+
 
