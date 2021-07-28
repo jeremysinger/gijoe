273221e690f5a288289870a871685a817812a9f1
Robbert Sinclair <2432710S@student.gla.ac.uk>
2021-07-28 16:53:02 +0100

    Returned all of the old files

--- custom/savefiles/1.js
+++ custom/savefiles/1.js
@@ -1,1 +1,18 @@
-/* Generic Javascript Code */
+// sample JavaScript code
+function fib(a) {
+  console.log(a);
+  if (a == 0) {
+    return 1;
+  } else if (a == 1) {
+    return 1;
+  } else {
+    return fib(a-1) + fib(a-2);
+  }
+}
+
+console.log(fib(2));
+
+
+
+
+
