2d274bd4e2f2d6bdc4ea4a7f46c89236a8d23989
ddrago <dragodiego286@gmail.com>
2021-08-19 10:45:07 +0100

    Everytime a user resets the editor, the editor saves the change

--- custom/savefiles/1.js
+++ custom/savefiles/1.js
@@ -1,13 +1,1 @@
-var helloWorld = document.getElementById("hello-world");
-
-helloWorld.innerHTML = "<h1 id='new-heading'>Hello World</h1>";
-helloWorld.style.color = "red";
-helloWorld.style.backgroundColor = "blue";
-helloWorld.style.padding = "10px";
-var newHeading = document.getElementById("new-heading");
-newHeading.style.backgroundColor = "yellow";
-newHeading.style.padding = "7px";
-newHeading.style.textAlign = "center";
-
-
-
+// GENERIC INITIALCODE FILE
