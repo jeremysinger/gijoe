f191ea075495c10b33f1f64f5adfe9ba6dbec1f9
ddrago <dragodiego286@gmail.com>
2021-08-26 11:26:30 +0100

    Now the outputblock has correct padding in the case there is no extra content to its left

--- public/js/outputResize.js
+++ public/js/outputResize.js
@@ -1,5 +1,6 @@
 const codeOutput = document.getElementById("fakeConsole");
 const consoleOutput = document.getElementById("output");
+const outputBlock = document.getElementById("outputblock")
 
 function resizeCodeOutput() {
     var new_height = consoleOutput.style.height;
@@ -11,5 +12,15 @@ function resizeConsoleOutput() {
     consoleOutput.style.height = new_height;
 }
 
+/**If the settings make it so that there is no extra info alongside the console, have the console still look nice
+ * and have extra padding on the left
+ */
+function outputPaddingAdjuster() {
+    if (!tutorialSettings.libraries.turtle && !tutorialSettings.libraries.DOM && !tutorialSettings.libraries.csv && !tutorialSettings.libraries.csvAndTurtle) {
+        outputBlock.style.paddingLeft = "40px";
+    } 
+}
+
 new ResizeObserver(resizeCodeOutput).observe(consoleOutput);
 new ResizeObserver(resizeConsoleOutput).observe(codeOutput);
+outputPaddingAdjuster()
