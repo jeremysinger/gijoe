a15b849deee82f107094ac2de9f14ebd87de1cc6
ddrago <dragodiego286@gmail.com>
2021-08-29 23:25:24 +0100

    Now progress bar is screen-width dependent

--- public/js/progressbar.js
+++ public/js/progressbar.js
@@ -2,12 +2,30 @@ const  Btnprevious  =  document.getElementById('tutorialBack');
 const  Btnnext  =  document.getElementById('tutorialForward');
 // const  Btnsubmit  =  document.getElementById('Submit');
 const  bullets  =  [...document.querySelectorAll('.bullets')];
+const pb = document.getElementById("pb");
 
 let current = 0;
 const max = 2;
+// let barLength = bullets.style.
+
 
 Btnnext.addEventListener('click',  ()  =>  {
     bullets[current].classList.add('completed');
+    if(current != max){
+        let currentX = bullets[current].getBoundingClientRect().left;
+        let nextX = bullets[current+1].getBoundingClientRect().left;
+        let lineWidth = nextX - currentX - 24; 
+    
+        // Create a new element to add
+        const line = document.createElement("div");
+        // line.innerHTML = "<h4>new node</h4>";
+        line.classList.add("line");
+        line.style.width = lineWidth + "px";
+    
+        // Insert the created element
+        bullets[current].after(line);
+    }
+
     current  +=  1;
     Btnprevious.style.display  =  'inline';
 });
