96b292aec5cec694580f087f8d0653c5ec63491c
ddrago <dragodiego286@gmail.com>
2021-08-30 01:12:42 +0100

    Made progress bar number of steps dependant on the number of tutorial files

--- public/js/progressbar.js
+++ public/js/progressbar.js
@@ -1,15 +1,16 @@
 const  Btnprevious  =  document.getElementById('tutorialBack');
 const  Btnnext  =  document.getElementById('tutorialForward');
 // const  Btnsubmit  =  document.getElementById('Submit');
-const  bullets  =  [...document.querySelectorAll('.bullets')];
 const pb = document.getElementById("pb");
 
 let current = 0;
-const max = 2;
+const max = tutorialFiles+1;
 // let barLength = bullets.style.
 
 
 Btnnext.addEventListener('click',  ()  =>  {
+    const  bullets  =  [...document.querySelectorAll('.bullets')];
+
     bullets[current].classList.add('completed');
     if(current != max){
         let currentX = bullets[current].getBoundingClientRect().left;
@@ -29,7 +30,8 @@ Btnnext.addEventListener('click',  ()  =>  {
     Btnprevious.style.display  =  'inline';
 });
 
-Btnprevious.addEventListener('click',  ()  =>  {
+Btnprevious.addEventListener('click',  ()  =>  {    
+    const  bullets  =  [...document.querySelectorAll('.bullets')];
 
     if(current!=max+1){
         //get rid of completed line
@@ -49,13 +51,30 @@ Btnprevious.addEventListener('click',  ()  =>  {
         bullets[current-1].appendChild(line);
     }
 
+    // if(current!=max+1){
+
+    //     const test = bullets[current-1].childNodes[1];
+    //     test.classList.add("emptyline");
+    //     test.classList.remove("line");
+    // }
+
     bullets[current  -  1].classList.remove('completed');
     current  -=  1;
 });
 
+function addPb(){
+    let step = "";
+    for(let i = 0; i<tutorialFiles; i++){
+        step = step + "<div class=\"step\"><div class=\"bullets\">" + (i+1) + "</div></div>";
+    }
+
+    pb.innerHTML = step;
+}
+
 function addEmptyLines(){
+    const  bullets  =  [...document.querySelectorAll('.bullets')];
+
     for(let i = 0; i < bullets.length-1; i++){
-        console.log(i+1);
         //add empty progress line
         let currentX = bullets[i].getBoundingClientRect().left;
         let nextX = bullets[i+1].getBoundingClientRect().left;
@@ -65,7 +84,6 @@ function addEmptyLines(){
         const line = document.createElement("div");
         line.classList.add("emptyline");
         line.style.width = lineWidth + "px";
-        console.log(i+1);
     
         bullets[i].appendChild(line);
     }
@@ -75,5 +93,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+addPb();
 sleep(5).then(addEmptyLines);
 
